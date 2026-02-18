ae848371e94acbc495cf8c8ef965589c6765b140
hexa4 <wunderleds@gmail.com>
2026-02-18 16:34:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,211 +1,121 @@
-// server.js
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
 const PORT = process.env.PORT || 3000;
-
 app.use(express.static('public'));
-
 let players = [];
 const connectedUsers = new Set();
-
-
-
-///let todosVertices = [];
-
-
-	/////////
-	
-	/*
-		const hexagonMap = [
-        [{ direction: 'NE' }, { direction: 'E' }, { direction: 'SE' }, { direction: 'E' }, { direction: 'SE' }, { direction: 'E' }, { direction: 'SE' }, { direction: 'E' }],
-        [{ direction: 'NW' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }],
-        [{ direction: 'NW' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }],
-        [{ direction: 'SW' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }],
-        [{ direction: 'SW' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }],
-        [{ direction: 'SW' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }]
-    ];
-    
-    
-    function generateAllHexagonVertices() {
-const hexagonSize = 50;
-const numRows = hexagonMap.length;
-const numCols = hexagonMap[0].length;
-const hexWidth = hexagonSize * Math.sqrt(3);
-const hexHeight = hexagonSize * Math.sqrt(3);
-const vertices = [];
-// Generar coordenadas medias entre los vértices adyacentes
-for (let row = 0; row < numRows; row++) {
-for (let col = 0; col < numCols; col++) {
-const x = col * (hexWidth * 0.87);
-const y = row * hexHeight + (col % 2 === 1 ? hexHeight / 2 : 0);
-const points = getHexagonPoints(x, y, hexagonSize);
-for (let i = 0; i < points.length; i++) {
-vertices.push(points[i]);
-todosVertices.push(points[i]);
-}}}
-return vertices;
-}
-const allHexagonVertices = generateAllHexagonVertices();
-
-    
-    function getHexagonPoints(x, y, size) {
-const points = [];
-for (let i = 0; i < 6; i++) {
-const angle = (2 * Math.PI / 6) * i;
-const pointX = x + size * Math.cos(angle);
-const pointY = y + size * Math.sin(angle);
-points.push({ x: pointX, y: pointY });
-}return points;}
-    
-    
-        
-function printRandomValue(min, max) {
-const randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
-return randomValue;
-}  
-*/
-
 
 //////////////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////   
 ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////  
 //////////////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////   
 ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////  
 //GREEN BALLS !!!!!!!!!!!!!
-              const hexagonMap = [
+const hexagonMap = [
         [{ direction: 'NE' }, { direction: 'E' }, { direction: 'SE' }, { direction: 'E' }],
         [{ direction: 'SW' }, { direction: 'ES' } ,{ direction: 'E' }, { direction: 'ES' }, { direction: 'E' }],
         [{ direction: 'SW' }, { direction: 'ES' }, { direction: 'E' }, { direction: 'ES' }, { direction: 'E' }]
-    ];
+];
     
 const availableColors = ['blue', 'purple', 'orange', 'pink', 'yellow', 'cyan', 'teal', 'maroon', 'lime', 'brown', 'indigo', 'gray', 'gold', 'silver', 'olive', 'navy', 'magenta', 'peach', 'violet', 'turquoise', 'lavender', 'salmon', 'beige'];
-
-
 const assignedColors = new Map();    
 let colorIndex = 0;
-
 let greenCirclesS = [];
 
-
-
-
 function getHexagonPoints(x, y, size) {
-const points = [];
-for (let i = 0; i < 6; i++) {
-const angle = (2 * Math.PI / 6) * i;
-const pointX = x + size * Math.cos(angle);
-const pointY = y + size * Math.sin(angle);
-points.push({ x: pointX, y: pointY });
-}return points;}
-function generateRandomLineCoordinates() {
-const hexagonSize = 50;
-const numRows = hexagonMap.length;
-const numCols = hexagonMap[0].length;
-const hexWidth = hexagonSize * Math.sqrt(3);
-const hexHeight = hexagonSize * Math.sqrt(3);
-const coordinates = [];
-// Generar coordenadas medias entre los vértices adyacentes
-for (let row = 0; row < numRows; row++) {
-for (let col = 0; col < numCols; col++) {
-const x = col * (hexWidth * 0.87);
-const y = row * hexHeight + (col % 2 === 1 ? hexHeight / 2 : 0);
-const points = getHexagonPoints(x, y, hexagonSize);
-for (let i = 0; i < points.length; i++) {
-const nextIndex = (i + 1) % points.length;
-const point1 = points[i];
-const point2 = points[nextIndex];
-const midX = (point1.x + point2.x) / 2;
-const midY = (point1.y + point2.y) / 2;
-const randomFactor = Math.random(); // Factor aleatorio entre 0 y 1
-const randomX = point1.x + (point2.x - point1.x) * randomFactor;
-const randomY = point1.y + (point2.y - point1.y) * randomFactor;
-if (!isNaN(midX) && !isNaN(midY)) {
-coordinates.push({ x: randomX, y: randomY, z: 0 });
-//console.log('randomX:', randomX, randomY);  
-}}}}	
-const randomCoordinates = new Set(); // Usamos un conjunto para evitar duplicados
-let index = 200; // Inicializamos el índice en 0
-while (randomCoordinates.size < 20) {
-const randomIndex = Math.floor(Math.random() * coordinates.length);
-const randomCoordinate = coordinates[randomIndex];
-// Verificar si randomCoordinate está definido
-if (randomCoordinate) {
-randomCoordinate.index = index; // Asignamos el índice a randomCoordinate
-randomCoordinates.add(randomCoordinate); // Agregamos la coordenada al conjunto
-// Agregar un console.log para imprimir las coordenadas aleatorias seleccionadas
-// console.log(`Coordenada aleatoria ${randomCoordinate.index}: (${randomCoordinate.x}, ${randomCoordinate.y}), ${randomCoordinate.z}, ${randomCoordinate.index}`);
-index++; 
-} else {
-console.log(`Error: No se pudo obtener la coordenada aleatoria ${randomCoordinates.size + 1}`); }
-}
-return Array.from(randomCoordinates); // Convertimos el conjunto a un array para mantener el formato de salida
-}
-greenCirclesS = generateRandomLineCoordinates();
-for (let i = 0; i < greenCirclesS.length; i++) {
-//console.log(`Valor Z MODIFICA: ${greenCirclesS[i].x}:`);
-greenCirclesS[i].z = i+1;
+	const points = [];
+	for (let i = 0; i < 6; i++) {
+		const angle = (2 * Math.PI / 6) * i;
+		const pointX = x + size * Math.cos(angle);
+		const pointY = y + size * Math.sin(angle);
+		points.push({ x: pointX, y: pointY });
+	}
+	return points;
 }
 
+function generateRandomLineCoordinates() {
+	const hexagonSize = 50;
+	const numRows = hexagonMap.length;
+	const numCols = hexagonMap[0].length;
+	const hexWidth = hexagonSize * Math.sqrt(3);
+	const hexHeight = hexagonSize * Math.sqrt(3);
+	const coordinates = [];
+	// Generar coordenadas medias entre los vértices adyacentes
+	for (let row = 0; row < numRows; row++) {
+		for (let col = 0; col < numCols; col++) {
+			const x = col * (hexWidth * 0.87);
+			const y = row * hexHeight + (col % 2 === 1 ? hexHeight / 2 : 0);
+			const points = getHexagonPoints(x, y, hexagonSize);
+			for (let i = 0; i < points.length; i++) {
+				const nextIndex = (i + 1) % points.length;
+				const point1 = points[i];
+				const point2 = points[nextIndex];
+				const midX = (point1.x + point2.x) / 2;
+				const midY = (point1.y + point2.y) / 2;
+				const randomFactor = Math.random(); // Factor aleatorio entre 0 y 1
+				const randomX = point1.x + (point2.x - point1.x) * randomFactor;
+				const randomY = point1.y + (point2.y - point1.y) * randomFactor;
+				if (!isNaN(midX) && !isNaN(midY)) {
+					coordinates.push({ x: randomX, y: randomY, z: 0 });
+					//console.log('randomX:', randomX, randomY);  
+				}
+			}
+		}
+	}	
+	
+	const randomCoordinates = new Set(); // Usamos un conjunto para evitar duplicados
+	let index = 200; // Inicializamos el índice en 0
+	while (randomCoordinates.size < 20) {
+		const randomIndex = Math.floor(Math.random() * coordinates.length);
+		const randomCoordinate = coordinates[randomIndex];
+		// Verificar si randomCoordinate está definido
+		if (randomCoordinate) {
+			randomCoordinate.index = index; // Asignamos el índice a randomCoordinate
+			randomCoordinates.add(randomCoordinate); // Agregamos la coordenada al conjunto
+			// Agregar un console.log para imprimir las coordenadas aleatorias seleccionadas
+			// console.log(`Coordenada aleatoria ${randomCoordinate.index}: (${randomCoordinate.x}, ${randomCoordinate.y}), ${randomCoordinate.z}, ${randomCoordinate.index}`);
+			index++; 
+		} else {
+			console.log(`Error: No se pudo obtener la coordenada aleatoria ${randomCoordinates.size + 1}`); 
+		}
+	}
+	return Array.from(randomCoordinates); // Convertimos el conjunto a un array para mantener el formato de salida
+} //END generateRandomLineCoordinates
+greenCirclesS = generateRandomLineCoordinates();
+
+for (let i = 0; i < greenCirclesS.length; i++) {
+	//console.log(`Valor Z MODIFICA: ${greenCirclesS[i].x}:`);
+	greenCirclesS[i].z = i+1;
+}
 console.log(`LENGTH GreenCirclesS: ${greenCirclesS.length}:`);	
 
-
-
-
-
 //////////////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////   
 ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////  
 //////////////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////   
 ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////    ///////  
-
-
-
 
 
 io.on('connection', (socket) => {
     console.log('Nuevo jugador conectado');
-    
     connectedUsers.add(socket.id);
-
     console.log(`Total de usuarios: ${connectedUsers.size}`);
-    
 	io.emit('userCount', connectedUsers.size);
 	
-	//RECIBIR LLAMADA GREENCIRCLESS DE CLIENTE 
-  socket.on('LlamargreenCirclesS', () => {
-      console.log(`LLAMAR GREEN CIR:`);
-		//ENVIAR GREENCIRCLES A CLIENTE DESDES SERVER
-           socket.emit('greenCirclesS', greenCirclesS);
-    });
-    
-	
-//OBTENER COLOR PARA JUGADOR    
-const colorsArray = Array.from(availableColors);
-const userColor = colorsArray[colorIndex % colorsArray.length];
-colorIndex++;
-assignedColors.set(socket.id, { color: userColor});
-    
-
-        
-
+	//OBTENER COLOR PARA JUGADOR    
+	const colorsArray = Array.from(availableColors);
+	const userColor = colorsArray[colorIndex % colorsArray.length];
+	colorIndex++;
+	assignedColors.set(socket.id, {color: userColor});
+       
     socket.on('disconnect', () => {
-    connectedUsers.delete(socket.id);
-assignedColors.delete(socket.id);
-console.log('Jugador desconectado');
-        
-
-    });
-    
-
-    
+    	connectedUsers.delete(socket.id);
+		assignedColors.delete(socket.id);
+		console.log('Jugador desconectado');
+    });	
 });
-
-
 
 
 server.listen(PORT, () => {
